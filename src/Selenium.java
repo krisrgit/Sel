32caeeb36a70f53a17eba087fbc3be583b0bc60c
krisrgit <krishnakumargitr@gmail.com>
2021-04-21 19:57:23 +0530

    This is branch checkin

--- src/Selenium.java
+++ src/Selenium.java
@@ -4,5 +4,7 @@ public class Selenium {
 	public static void main(String[] args) {
 		System.out.println("This is new project");
 		
+		System.out.println (" This is commit only to branch");
+		
 	}
 }
